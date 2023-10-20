68df6e3aece1d12b287079c639dfac692c87ed44
Sinx <gabriel.jbrito2@gmail.com>
2023-10-20 16:12:30 -0300

    Configurando controladores da API

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,20 +1,36 @@
 ﻿using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace FilmesAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class FilmeController : ControllerBase
     {
-        private List<Filme> filmes = new List<Filme>();
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
         }
+
+        [HttpGet]
+        public IEnumerable<Filme> GetFilmes([FromQuery] int skip =0, [FromQuery] int take =10) //Aqui diz que se o usuário n passar parametros, carregará 50 filmes. Se quiser recarregar tudo, basta tirar o skip e o take
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public Filme? GetFilmeId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
+        }
+
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,11 +1,24 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
+
+
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="O título do filme é Obrigatório")]
         public string Titulo { get; set; }
 
+        [Required(ErrorMessage = "O Gênero do filme é Obrigatório"),
+            MaxLength(50,ErrorMessage ="O tamanho do gênero não pode exceder 50 caracteres.")]
         public string Genero { get; set; }
 
+        [Required(ErrorMessage = "A duração do filme é Obrigatória"),
+            Range(70,600,ErrorMessage ="A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
+        
     }
 }
